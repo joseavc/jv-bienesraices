d4cfcec07b71cd3dbf74d478093c8bc899410316
joseavc <joseavillalobos1997@gmail.com>
2022-03-29 23:16:49 -0600

    Postgres configuracion

--- jvbienesraices/settings.py
+++ jvbienesraices/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'jvbienesraices.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'jvbienesraices_db',
+        'USER': 'JOSEAVC',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
